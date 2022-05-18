fce1e4edef6cadf00646ec0521c4fe255eeddf5d
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-05-17 21:22:18 -0400

    check

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  const App({ Key? key }) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,3 +17,4 @@ class App extends StatelessWidget {
     );
   }
 }
+// just a check 
