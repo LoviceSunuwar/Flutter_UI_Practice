18049f285f8338467c5b83e4e02497407d68f390
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-08-10 22:22:31 -0400

    continue

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHeight, _deviceWidth;
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage("assets/images/abc.png"),
-          ),
+      body: SafeArea(
+        child: Container(
+          height: _deviceHeight,
+          width: _deviceWidth,
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          child: _simiansTitle(),
+        ),
+      ),
+    );
+  }
+
+  Widget _simiansTitle() {
+    return const Text(
+      "OG Simians",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _simainsbody() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("assets/images/abc.png"),
         ),
       ),
     );
